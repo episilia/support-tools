3fdc5355ff6a7b2bb30b42c174f563b3aa917cc2
Engineering 91 <engineering@91social.com>
2024-07-24 14:12:17 +0530

    adding updated bash script file to deploy grafana

--- Grafana/grafana_install.sh
+++ Grafana/grafana_install.sh
@@ -1,37 +1,44 @@
 #! /bin/bash
-read -p "Please enter the Grafana version that you want to deploy: " VERSION
 
-case $VERSION in
-    8)
-        echo "Deploying grafana version v8.0.5"
+echo "Select the Grafana version that you want to deploy:"
+echo "1) Grafana v8.0.5"
+echo "2) Grafana v10.0.12"
+echo "3) Exit"
 
-        echo "reading the namespace from user"
-        read -p "Please enter the namespace: " name
+read -p "Enter the number corresponding to your choice: " choice
 
-        echo "checking and creating the namespace"
-        kubectl get ns $name || kubectl create namespace $name
-
-        echo "Deploying grafana"
-        kubectl create deployment grafana --image=docker.io/grafana/grafana:8.0.5-ubuntu -n $name
-        kubectl expose deployment grafana --type=LoadBalancer --port=80 --target-port=3000 --protocol=TCP -n $name
-        echo
+case $choice in
+    1)
+        VERSION="8.0.5-ubuntu"
+        echo "You have selected Grafana version v8.0.5"
         ;;
-    10)
-        echo "Deploying grafana version v10.0.12"
-
-        echo "reading the namespace from user"
-        read -p "Please enter the namespace: " name
-
-        echo "checking and creating the namespace"
-        kubectl get ns $name || kubectl create namespace $name
-
-        echo "Deploying grafana"
-        kubectl create deployment grafana --image=docker.io/grafana/grafana:10.0.12-ubuntu -n $name
-        kubectl expose deployment grafana --type=LoadBalancer --port=80 --target-port=3000 --protocol=TCP -n $name
-        echo
+    2)
+        VERSION="10.0.12-ubuntu"
+        echo "You have selected Grafana version v10.0.12"
+        ;;
+    3)
+        echo "Exiting..."
+        exit 0
         ;;
     *)
-        echo "Sorry selected grafana version $VERSION is not available."
-        echo  
+        echo "Invalid choice. Exiting..."
+        exit 1
         ;;
 esac
+
+echo "Reading the namespace from user"
+read -p "Please enter the namespace (default if not specified): " name
+name=${name:-default}
+
+if [ "$name" != "default" ]; then
+    echo "Checking and creating the namespace if it does not exist"
+    kubectl get ns $name || kubectl create namespace $name
+    namespace_option="-n $name"
+else
+    namespace_option=""
+fi
+
+echo "Deploying Grafana version $VERSION"
+kubectl create deployment grafana --image=docker.io/grafana/grafana:$VERSION $namespace_option
+kubectl expose deployment grafana --type=LoadBalancer --port=80 --target-port=3000 --protocol=TCP $namespace_option
+echo "Deployment complete."
