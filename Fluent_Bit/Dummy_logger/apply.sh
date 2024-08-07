d9e9d36ad969254b025a55429bcacdc3adaee86e
Bhargav <147476165+bhargav-l@users.noreply.github.com>
2024-08-07 14:19:22 +0530

    Update apply.sh

--- Fluent_Bit/Dummy_logger/apply.sh
+++ Fluent_Bit/Dummy_logger/apply.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 kubectl apply -f Fluent-Bit_RBAC.yml
-kubectl apply -f dummy_logger.yml
+kubectl apply -f Dummy_logger.yml
 kubectl apply -f Fluent-Bit_Config.yml
 kubectl apply -f Fluent-Bit.yml
